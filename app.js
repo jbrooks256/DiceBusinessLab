9f93f7c3a619312c4f618432087c54db4c835b23
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-06-09 15:33:19 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let diceArr = [];
+let diceNumber = 0;
+
+$("#generateDie").click(() => new Die());
+$("#rollDie").click(() => console.log(diceArr));
+
+class Die {
+  constructor(value) {
+    this.value = value;
+    this.div = $("<div>");
+    $(this.div).addClass("die");
+    $(this.div).addClass(`${diceNumber++}`);
+    this.roll();
+    $(this.div).text(this.value);
+    $(".diceGame").append(this.div);
+    diceArr.push(this);
+  }
+
+  roll() {
+    let randomValue = Math.floor(Math.random() * 6 + 1);
+    this.value = randomValue;
+    $(this.div).text(this.value);
+  }
+}
